974d81715965b500a06f00d795980b997b9061d4
Sami Malik <sami.malik@appnovation.com>
2021-01-28 15:59:35 -0500

    Fix for handeling the storage of media type entities.

---
+++ src/Storage/Media/MediaStorage.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Drupal\discoverable_entity_bundle_classes\Storage\Media;
+
+use Drupal\discoverable_entity_bundle_classes\Storage\SqlContentEntityStorageTrait;
+use Drupal\media\MediaStorage as MediastorageBase;
+use Drupal\Core\Entity\ContentEntityStorageInterface;
+
+/**
+ * Defines the Media Storage class for discoverable entity bundle classes.
+ */
+class MediaStorage extends MediaStorageBase implements ContentEntityStorageInterface {
+
+  use SqlContentEntityStorageTrait;
+
+}
